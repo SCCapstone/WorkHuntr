3a1b6f1cec020df7c27be1ac947bc483e002f1b0
Noah <noahshaw11@yahoo.com>
2020-10-20 20:07:32 -0400

    Initial navigation complete

---
+++ workhuntr/workhuntr/urls.py
@@ -0,0 +1,28 @@
+"""workhuntr URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('login', views.login),
+    path('create_account', views.create_account),
+    path('dashboard', views.dashboard),
+    path('current_listings', views.current_listings),
+    path('modify_listings', views.modify_listings),
+    path('profile', views.profile)
+]
---
+++ workhuntr/workhuntr/views.py
@@ -0,0 +1,29 @@
+from django.core.exceptions import *
+from django.shortcuts import render, redirect, HttpResponse
+from django.template import loader
+from django.views.decorators.csrf import csrf_exempt
+import requests
+
+@csrf_exempt
+def login(request):
+  return render(request, 'login.html')
+
+@csrf_exempt
+def create_account(request):
+  return render(request, 'create_account.html')
+
+@csrf_exempt
+def dashboard(request):
+  return render(request, 'dashboard.html')
+
+@csrf_exempt
+def current_listings(request):
+  return render(request, 'current_listings.html')
+
+@csrf_exempt
+def modify_listings(request):
+  return render(request, 'modify_listings.html')
+
+@csrf_exempt
+def profile(request):
+  return render(request, 'profile.html')
