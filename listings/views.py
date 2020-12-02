b321fd453dfc1a7cad7ae56c7db2e1318ba691be
Timothy Bollman <tbollman92@gmail.com>
2020-12-02 11:31:07 -0500

    Modify Listing Form is added

--- listings/views.py
+++ listings/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
 # Create your views here.
-
+@login_required
 def create_listings(request):
     listings = Listings.objects.all()
 
@@ -19,6 +19,7 @@ def create_listings(request):
             listing.title = form.cleaned_data.get('title')
             listing.description = form.cleaned_data.get('description')
             listing.data = form.cleaned_data.get('date')
+            listing.status = form.cleaned_data.get('status')
             listing.save()
             messages.success(request, f'The Listing has been created!')
             return redirect('/current_listings/')
@@ -28,23 +29,31 @@ def create_listings(request):
     context = {'listings':listings, 'form':form}
     return render(request, 'listings/create_listings.html', context)
 
+@login_required
 def current_listings(request):
     listings = Listings.objects.all()
     context = {'listings': listings}
     return render(request, 'listings/current_listings.html', {'listings': listings})
 
+@login_required
 def modify_listings(request, pk):
     listing = Listings.objects.get(id=pk)
     form = ModifyListingForm(instance=listing)
 
     if request.method == 'POST':
         if form.is_valid():
-            form.save()
+            listing = form.save()
+            listing.refresh_from_db()
+            listing.title = form.cleaned_data.get('title')
+            listing.status = form.cleaned_data.get('status')
+            listing.save()
+            messages.success(request, f'Your listing has been updated!')
         return redirect('/current_listings/')
 
     context = {'form':form}
-    return render(request, 'listings/modify_listings.html')
+    return render(request, 'listings/modify_listings.html', context)
 
+@login_required
 def delete_listing(request,pk):
     item = Listings.objects.get(id=pk)
 
@@ -53,4 +62,4 @@ def delete_listing(request,pk):
         return redirect('/current_listings/')
 
     context = {'item':item}
-    return render(request, 'listings/delete_listing.html')
+    return render(request, 'listings/delete_listing.html', context)
