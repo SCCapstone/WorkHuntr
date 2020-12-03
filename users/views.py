e99d855060ed3b0dfa89290404ac8673d9e394fa
Noah Shaw <noahshaw11@yahoo.com>
2020-12-03 04:05:00 -0500

    Update views.py

--- users/views.py
+++ users/views.py
@@ -7,75 +7,67 @@ from .forms import AddCommentForm, ProfileUpdateForm, UserCreateAccountForm, Use
 from .models import Comment, Profile
 
 def create_account(request):
-  if request.method == 'POST':
-    form = UserCreateAccountForm(request.POST)
-    if form.is_valid():
-      user = form.save()
-      user.refresh_from_db()
-      user.profile.email = form.cleaned_data.get('email')
-      user.profile.gender = form.cleaned_data.get('gender')
-      user.profile.title = form.cleaned_data.get('title')
-      user.profile.account_type = form.cleaned_data.get('account_type')
-      user.profile.save()
-      messages.success(request, f'Your account has been created! You can now login.')
-      return redirect('login')
-  else:
-    form = UserCreateAccountForm()
-  return render(request, 'users/create_account.html', {'form': form})
+    if request.method == 'POST':
+        form = UserCreateAccountForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user.refresh_from_db()
+            user.profile.email = form.cleaned_data.get('email')
+            user.profile.gender = form.cleaned_data.get('gender')
+            user.profile.title = form.cleaned_data.get('title')
+            user.profile.account_type = form.cleaned_data.get('account_type')
+            user.profile.save()
+            messages.success(request, f'Your account has been created! You can now login.')
+            return redirect('login')
+    else:
+        form = UserCreateAccountForm()
+    return render(request, 'users/create_account.html', {'form': form})
 
 @login_required
 def profile(request, username):
-  if request.method == 'POST':
-    search_user = request.POST.get('search', None)
-    if search_user == '':
-      return redirect('profile', username)
-    return redirect('profile', search_user)
-  user = User.objects.get(username=username)
-  viewable = True
-  editable = True
-  can_comment = False
-  if request.user != user and request.user.profile.account_type == 'Huntee':
-    editable = False
-    if user.profile.privacy == 'Private':
-      viewable = False
-    can_comment = True
-  comments = Comment.objects.filter(profile=user.profile)
-  return render(request, 'users/profile.html', {'user':user, 'viewable':viewable, 'editable':editable, 'can_comment':can_comment, 'comments':comments})
+    if request.method == 'POST':
+        search_user = request.POST.get('search', None)
+        if search_user == '':
+            return redirect('profile', username)
+        return redirect('profile', search_user)
+    user = User.objects.get(username=username)
+    comments = Comment.objects.filter(profile=user.profile)
+    return render(request, 'users/profile.html', {'user':user})
 
 @login_required
 def edit_profile(request, username):
-  if request.method == 'POST':
-    u_form = UserUpdateForm(request.POST, instance=request.user)
-    p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
-    if u_form.is_valid() and p_form.is_valid():
-      u_form.save()
-      p_form.save()
-      messages.success(request, f'Your account has been updated!')
-      return redirect('profile', username)
-  else:
-    u_form = UserUpdateForm(instance=request.user)
-    p_form = ProfileUpdateForm(instance=request.user.profile)
-  context = {
-    'u_form': u_form,
-    'p_form': p_form
-  }
-  return render(request, 'users/edit_profile.html', context)
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your account has been updated!')
+        return redirect('profile', username)
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+    return render(request, 'users/edit_profile.html', context)
 
 @login_required
 def add_comment(request, username):
-  if request.method == 'POST':
-    form = AddCommentForm(request.POST)
-    if form.is_valid():
-      comment = form.save(commit=False)
-      comment.comment = form.cleaned_data.get('comment')
-      comment.rating = form.cleaned_data.get('rating')
-      comment.company = form.cleaned_data.get('company')
-      profile_user = User.objects.get(username=username)
-      comment.profile = profile_user.profile
-      comment.author = request.user
-      comment.save()
-      messages.success(request, f'Your comment has been added!')
-      return redirect('profile', username)
-  else:
-    form = AddCommentForm()
-  return render(request, 'users/comment.html', {'comment_form': form})
+    if request.method == 'POST':
+        form = AddCommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.comment = form.cleaned_data.get('comment')
+            comment.rating = form.cleaned_data.get('rating')
+            comment.company = form.cleaned_data.get('company')
+            profile_user = User.objects.get(username=username)
+            comment.profile = profile_user.profile
+            comment.author = request.user
+            comment.save()
+            messages.success(request, f'Your comment has been added!')
+            return redirect('profile', username)
+    else:
+        form = AddCommentForm()
+    return render(request, 'users/comment.html', {'comment_form': form})
