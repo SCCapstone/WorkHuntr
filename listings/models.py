11b25c7cb273d8b36fff00e5c5f14c82ad7b4abd
Noah Shaw <noahshaw11@yahoo.com>
2020-12-03 03:42:53 -0500

    Update models.py

--- listings/models.py
+++ listings/models.py
@@ -1,31 +1,29 @@
-import datetime
-
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+import datetime
 import random
 import string
-# Create your models here.
+
+STATUSES = [
+    ('Strutting', 'Strutting'),
+    ('Spotted', 'Spotted'),
+    ('Claimed', 'Claimed'),
+    ('Completed', 'Completed')
+]
+
+TAGS = [
+    ('NONE', 'NONE'),
+    ('AI', 'AI'),
+    ('DEVELOPMENT', 'DEVELOPMENT'),
+    ('DESIGN', 'DESIGN'), 
+    ('C++', 'C++'),
+    ('Java', 'Java'),
+    ('Python', 'Python'),
+    ('Web', 'Web')
+]
 
 class Listings(models.Model):
-    STATUSES = [
-        ('Strutting', 'Strutting'),
-        ('Spotted', 'Spotted'),
-        ('Claimed', 'Claimed'),
-        ('Completed', 'Completed')
-    ]
-
-    TAGS = [
-        ('NONE', 'NONE'),
-        ('AI', 'AI'),
-        ('DEVELOPMENT', 'DEVELOPMENT'),
-        ('DESIGN', 'DESIGN'), 
-        ('C++', 'C++'),
-        ('Java', 'Java'),
-        ('Python', 'Python'),
-        ('Web', 'Web')
-    ]
-
     title = models.CharField(max_length=200)
     date = models.DateTimeField(auto_now_add=True)
     price = models.DecimalField(max_digits=6, decimal_places=2, default=0.00)
@@ -36,6 +34,7 @@ class Listings(models.Model):
     tag3 = models.CharField(max_length=11, choices=TAGS, default='NONE', verbose_name='TAG THREE')
     huntee = models.ForeignKey(User, on_delete=models.CASCADE, default=1, related_name='huntee')
     hunter = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name='hunter')
+    
     def __str__(self):
         return self.title
 
