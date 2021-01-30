4a05ee92ca67d7b6b8e8d83ccee0ce9416eac4ed
Matthewjre <matthewjre@yahoo.com>
2021-01-30 09:01:17 -0500

    Added UI test folder and a test for a successful & failed login

---
+++ UITesting/test_login.py
@@ -0,0 +1,60 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+
+# For testing a successful login
+
+# workHuntr test login
+USERNAME = "MatthewHuntee"
+PASSWORD = "Workhuntr1"
+
+driver = webdriver.Chrome("chromedriver.exe")
+
+# go to login page
+urlLogin = "http://3.238.55.75:8000/login/?next=/dashboard/"
+driver.get(urlLogin)
+# find username field
+driver.find_element_by_name("username").send_keys(USERNAME)
+# find password field
+driver.find_element_by_name("password").send_keys(PASSWORD)
+# login
+driver.find_element_by_id("loginButton").click()
+
+# wait for load
+WebDriverWait(driver=driver, timeout=20)
+
+url = driver.current_url
+
+if url == urlLogin:
+    print("LOGIN FAILED")
+else:
+    print("LOGIN SUCCESSFUL")
+
+# close
+driver.close()
+
+
+# For testing failed login
+
+USERNAME = "dfgdfgdfg"
+PASSWORD = "asdasdasd"
+
+driver = webdriver.Chrome("chromedriver.exe")
+
+urlLogin = "http://3.238.55.75:8000/login/?next=/dashboard/"
+driver.get(urlLogin)
+driver.find_element_by_name("username").send_keys(USERNAME)
+driver.find_element_by_name("password").send_keys(PASSWORD)
+driver.find_element_by_id("loginButton").click()
+
+WebDriverWait(driver=driver, timeout=500)
+
+url = driver.current_url
+
+if url == urlLogin:
+    print("LOGIN FAILED")
+else:
+    print("LOGIN SUCCESSFUL")
+
+# close
+driver.close()
+
