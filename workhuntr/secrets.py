2b0ded36a779a44720dc5aa074c716e945b158e3
tbollman <47295560+tbollman@users.noreply.github.com>
2021-04-05 01:55:08 -0400

    Update secrets.py

--- workhuntr/secrets.py
+++ workhuntr/secrets.py
@@ -1,6 +1,6 @@
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '!rp2$13ql2%c2^rk2!53wj13pox3wlw-a*8v)8zfnhr!yfo6)7'
 
-DEFAULT_FROM_EMAIL = 'giantnerds21@gmail.com'
-EMAIL_HOST_USER = 'giantnerds21@gmail.com'
-EMAIL_HOST_PASSWORD = 'G1antN3rds21'
+DEFAULT_FROM_EMAIL = 'postmaster@sandboxac31a25d8e2242e38e7abe84bce9d9cb.mailgun.org'
+EMAIL_HOST_USER = 'postmaster@sandboxac31a25d8e2242e38e7abe84bce9d9cb.mailgun.org'
+EMAIL_HOST_PASSWORD = 'b79ccb609a55969e1ecaf8789ab84d8a-e687bab4-deb329ca'
