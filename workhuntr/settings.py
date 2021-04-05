ce9db29d8721c2b174896fb463d148b1822a412f
tbollman <47295560+tbollman@users.noreply.github.com>
2021-04-05 00:27:52 -0400

    Modified Settings.py to include STATIC_ROOT

--- workhuntr/settings.py
+++ workhuntr/settings.py
@@ -17,7 +17,8 @@ from .secrets import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = '/static/'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
