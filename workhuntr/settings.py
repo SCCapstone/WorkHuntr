2e8ffc47f9668a5629cae4404c94729d563117ef
tbollman <47295560+tbollman@users.noreply.github.com>
2021-04-05 01:29:20 -0400

    Update settings.py

--- workhuntr/settings.py
+++ workhuntr/settings.py
@@ -136,6 +136,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGIN_URL = 'login'
 
+EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
