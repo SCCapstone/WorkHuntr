673d9737967293b240844e7692722fefeaa93126
jhill08 <jah18@email.sc.edu>
2021-04-19 21:38:31 -0400

    Fixed bugs in listings/views.py where hunter could return completed listings, huntee and hunter could view receipts for unpaid listings, hunters could complete listings that were not claimed, huntees could issue payments for incomplete and unclaimed listings, progress could be viewed before listing was started and after paid

--- listings/views.py
+++ listings/views.py
@@ -54,32 +54,35 @@ def current_listings(request):
 @login_required
 def progress(request, pk):
     listing = Listings.objects.get(id=pk)
-    if request.user == listing.hunter or request.user == listing.huntee:
-        listings = Listings.objects.all()
-        updates = Update.objects.all()
-        select_page = request.GET.get('selected', '')
-        if select_page:
-            item = Listings.objects.get(title=select_page)
+    if request.user == listing.hunter and listing.status != 'Strutting' or request.user == listing.huntee and listing.status != 'Strutting':
+        if request.user == listing.hunter and listing.status != 'Payment Issued' or request.user == listing.huntee and listing.status != 'Payment Issued':
+            listings = Listings.objects.all()
+            updates = Update.objects.all()
+            select_page = request.GET.get('selected', '')
+            if select_page:
+                item = Listings.objects.get(title=select_page)
+            else:
+                item = Listings.objects.get(id=pk)
+            form = UpdateForm(request.POST)
+            unread_messages = MessagingService.get_unread_messages(request, request.user)
+            has_unread_messages = True
+            num_of_unread_messages = unread_messages.count()
+            if not unread_messages:
+                has_unread_messages = False
+            context = {'listings': listings, 'updates': updates, 'item':item, 'form': form, 'has_unread_messages': has_unread_messages, 'num_of_unread_messages': num_of_unread_messages}
+            if request.method == 'POST':
+                if form.is_valid():
+                    update = form.save(commit=False)
+                    update.date = timezone.now()
+                    update.description = form.cleaned_data.get('description')
+                    update.status = form.cleaned_data.get('status')
+                    update.listing = item
+                    update.save()
+                    item.status = update.status
+                    item.save()
+            return render(request, 'listings/progress.html', context)
         else:
-            item = Listings.objects.get(id=pk)
-        form = UpdateForm(request.POST)
-        unread_messages = MessagingService.get_unread_messages(request, request.user)
-        has_unread_messages = True
-        num_of_unread_messages = unread_messages.count()
-        if not unread_messages:
-            has_unread_messages = False
-        context = {'listings': listings, 'updates': updates, 'item':item, 'form': form, 'has_unread_messages': has_unread_messages, 'num_of_unread_messages': num_of_unread_messages}
-        if request.method == 'POST':
-            if form.is_valid():
-                update = form.save(commit=False)
-                update.date = timezone.now()
-                update.description = form.cleaned_data.get('description')
-                update.status = form.cleaned_data.get('status')
-                update.listing = item
-                update.save()
-                item.status = update.status
-                item.save()
-        return render(request, 'listings/progress.html', context)
+            return redirect('current_listings')
     else:
         return redirect('current_listings')
 
@@ -138,16 +141,20 @@ def delete_listing(request, pk):
 @login_required
 def return_listing(request, pk):
     listing = Listings.objects.get(id=pk)
-    if request.user != listing.hunter and listing.status != 'Claimed':
+    if request.user == listing.hunter and listing.status != 'Payment Issued':
+        if request.user == listing.hunter and listing.status != 'Complete':
+            listing.status = 'Strutting'
+            listing.hunter = None
+            listing.save()
+            messages.success(request, f'Listing ' + str(listing.title) + ' has been returned to current listings. ' + str(listing.huntee) + ' has been notified!')
+            huntee = listing.huntee
+            content = 'Your listing ' + listing.title + ' has been returned to current listings by ' + str(listing.hunter) + '.'
+            MessagingService.send_message(request, sender=request.user, recipient=huntee, message=content)
+            return redirect('current_listings')
+        else:
+            return redirect('current_listings')
+    else:
         return redirect('current_listings')
-    listing.status = 'Strutting'
-    listing.hunter = request.user
-    listing.save()
-    messages.success(request, f'Listing ' + str(listing.title) + ' has been returned to current listings. ' + str(listing.huntee) + ' has been notified!')
-    huntee = listing.huntee
-    content = 'Your listing ' + listing.title + ' has been returned to current listings by ' + str(listing.hunter) + '.'
-    MessagingService.send_message(request, sender=request.user, recipient=huntee, message=content)
-    return redirect('current_listings')
 
 @login_required
 def claim_listing(request, pk):
@@ -166,42 +173,47 @@ def claim_listing(request, pk):
 @login_required
 def complete_listing(request, pk):
     listing = Listings.objects.get(id=pk)
-    if request.user != listing.huntee:
+    if request.user == listing.hunter and listing.status != 'Strutting':
+        if request.user == listing.hunter and listing.status != 'Payment Issued':
+            listing.status = 'Complete'
+            listing.save()
+            messages.success(request, f'Listing  ' + str(listing.title) + ' has been marked completed! ' + str(listing.huntee) + ' has been notified!')
+            huntee = listing.huntee
+            content = 'Your listing ' + listing.title + ' has been marked completed by ' + str(listing.hunter) + '! Please issue payment for completed listing.'
+            MessagingService.send_message(request, sender=request.user, recipient=huntee, message=content)
+            return redirect('current_listings')
+        else:
+            return redirect('current_listings')
+    else:
         return redirect('current_listings')
-    listing.status = 'Completed'
-    listing.save()
-    messages.success(request, f'Listing  ' + str(listing.title) + ' has been marked completed! ' + str(listing.huntee) + ' has been notified!')
-    huntee = listing.huntee
-    content = 'Your listing ' + listing.title + ' has been marked completed by ' + str(listing.hunter) + '! Please issue payment for completed listing.'
-    MessagingService.send_message(request, sender=request.user, recipient=huntee, message=content)
-    return redirect('current_listings')
 
 @login_required
 def issue_payment(request, pk):
     listing = Listings.objects.get(id=pk)
-    if request.user != listing.huntee:
-        return redirect('current_listings')
-    if request.method == "POST":
-        listing.status = 'Payment Issued'
-        listing.save()
-        messages.success(request, f'Payment has been issued to ' + str(listing.hunter) + ' in the amount of $' + str(listing.price) + '!')
-        hunter = listing.hunter
-        content = 'Receipt for ' + listing.title +  ' { Listing number:' +' (' + pk + ') ' + ', Listing Price: $' + str(listing.price) + ', Completed by: ' + str(listing.hunter) + ', Listed by: ' + str(listing.huntee) + ' }'
-        MessagingService.send_message(request, sender=request.user, recipient=hunter, message=content)
-        return redirect('current_listings')
+    if request.user == listing.huntee and listing.status == 'Complete':
+        if request.method == "POST":
+            listing.status = 'Payment Issued'
+            listing.save()
+            messages.success(request, f'Payment has been issued to ' + str(listing.hunter) + ' in the amount of $' + str(listing.price) + '!')
+            hunter = listing.hunter
+            content = 'Receipt for ' + listing.title +  ' { Listing number:' +' (' + pk + ') ' + ', Listing Price: $' + str(listing.price) + ', Completed by: ' + str(listing.hunter) + ', Listed by: ' + str(listing.huntee) + ' }'
+            MessagingService.send_message(request, sender=request.user, recipient=hunter, message=content)
+            return redirect('current_listings')
+        else:
+            unread_messages = MessagingService.get_unread_messages(request, request.user)
+            has_unread_messages = True
+            num_of_unread_messages = unread_messages.count()
+            if not unread_messages:
+                has_unread_messages = False
+            context = {'form': PaymentForm(), 'listing':listing, 'has_unread_messages':has_unread_messages, 'num_of_unread_messages':num_of_unread_messages}
+            return render(request, "listings/issue_payment.html", context)
     else:
-        unread_messages = MessagingService.get_unread_messages(request, request.user)
-        has_unread_messages = True
-        num_of_unread_messages = unread_messages.count()
-        if not unread_messages:
-            has_unread_messages = False
-        context = {'form': PaymentForm(), 'listing':listing, 'has_unread_messages':has_unread_messages, 'num_of_unread_messages':num_of_unread_messages}
-        return render(request, "listings/issue_payment.html", context)
+        return redirect('current_listings')
 
 @login_required
 def receipt(request, pk):
     listing = Listings.objects.get(id=pk)
-    if request.user == listing.huntee or request.user == listing.hunter:
+    if request.user == listing.huntee and listing.status == 'Payment Issued' or request.user == listing.hunter and listing.status == 'Payment Issued':
         if request.method == "POST":
             return redirect('current_listings')
         else:
