ad9cb24ba60fef95698be688560f3a0facd51187
Noah Shaw <noahshaw11@yahoo.com>
2021-04-20 21:26:16 -0400

    Update settings.py

--- workhuntr/settings.py
+++ workhuntr/settings.py
@@ -14,7 +14,6 @@ import os
 import django_heroku
 from pathlib import Path
 from typing import Sequence
-from .secrets import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
