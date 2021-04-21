e97d564f092ff4a55253a54b3c67ec2c5388268a
Noah Shaw <noahshaw11@yahoo.com>
2021-04-20 21:27:25 -0400

    Update settings.py

--- workhuntr/settings.py
+++ workhuntr/settings.py
@@ -22,7 +22,7 @@ STATIC_URL = '/static/'
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = SECRET_KEY
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
