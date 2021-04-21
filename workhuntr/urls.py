d973f115629f6c76fdd81b23ffc70eb5f8a50d54
Noah Shaw <noahshaw11@yahoo.com>
2021-04-20 22:49:10 -0400

    Update urls.py

--- workhuntr/urls.py
+++ workhuntr/urls.py
@@ -65,5 +65,4 @@ urlpatterns = [
     path('faq/', workhuntr_views.faq, name='faq')
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
