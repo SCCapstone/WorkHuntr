0b860906971c936074bc3fc4ea7e1e949fc4ca15
Noah Shaw <noahshaw11@yahoo.com>
2021-04-20 21:26:32 -0400

    Update settings.py

--- workhuntr/settings.py
+++ workhuntr/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 from typing import Sequence
-from .secrets import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
