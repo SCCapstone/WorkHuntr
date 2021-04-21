c81785f2a7b0b111df7877dc1a79b321e4da0d70
Noah Shaw <noahshaw11@yahoo.com>
2021-04-20 21:23:58 -0400

    Delete secrets.py

--- workhuntr/secrets.py
+++
@@ -1,6 +0,0 @@
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!rp2$13ql2%c2^rk2!53wj13pox3wlw-a*8v)8zfnhr!yfo6)7'
-
-DEFAULT_FROM_EMAIL = 'giantnerds21@gmail.com'
-EMAIL_HOST_USER = 'giantnerds21@gmail.com'
-EMAIL_HOST_PASSWORD = 'ljbztnawjiuegzdv'
