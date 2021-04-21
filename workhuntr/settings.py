ba96f54f27261dcf3bf9830d0375929b0474b02e
Noah Shaw <noahshaw11@yahoo.com>
2021-04-20 22:14:13 -0400

    Update settings.py

--- workhuntr/settings.py
+++ workhuntr/settings.py
@@ -17,7 +17,7 @@ from typing import Sequence
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
