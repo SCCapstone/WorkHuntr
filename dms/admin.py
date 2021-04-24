b8400f059100b83fc3f602e270e0de4206abd99b
Noah Shaw <noahshaw11@yahoo.com>
2021-04-24 13:10:00 -0400

    Update admin.py

--- dms/admin.py
+++ dms/admin.py
@@ -1,8 +1,13 @@
+#
+# Admin page configuration for Message
+#
+
 from django.contrib import admin
 from .models import Message
 
 class MessageAdmin(admin.ModelAdmin):
     model = Message
-    list_display = ('id', 'sender', 'content', )
+    list_display = ('id', 'sender', 'content')
 
+# Register the Message model on the Admin page
 admin.site.register(Message, MessageAdmin)
