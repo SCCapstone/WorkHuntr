4af138e0822ce000f6d18993754ff7d58bfeb359
Noah Shaw <noahshaw11@yahoo.com>
2021-04-24 13:37:18 -0400

    Update admin.py

--- listings/admin.py
+++ listings/admin.py
@@ -1,5 +1,11 @@
+#
+# Adming page configuration for Listings
+#
+
 from django.contrib import admin
 from .models import *
 
+# Register the Listing model on the Admin page
 admin.site.register(Listings)
+# Register the Update model on the Admin page
 admin.site.register(Update)
