8a900b7c87d0ffb9b5125af010650283a58892e5
Noah Shaw <noahshaw11@yahoo.com>
2021-04-24 14:17:18 -0400

    Update apps.py

--- users/apps.py
+++ users/apps.py
@@ -1,7 +1,12 @@
+#
+# Configure the Users app
+#
+
 from django.apps import AppConfig
 
 class UsersConfig(AppConfig):
     name = 'users'
-
+    
+    # Import the signals for the Users app
     def ready(self):
         import users.signals
