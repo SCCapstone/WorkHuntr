5b52e471fa5114749a696200407ce43acbb56752
Noah Shaw <noahshaw11@yahoo.com>
2021-04-25 15:01:34 -0400

    Update models.py

--- users/models.py
+++ users/models.py
@@ -2,6 +2,7 @@
 # Models for the Users app
 #
 
+from cloudinary.models import CloudinaryField
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import FileExtensionValidator
@@ -39,7 +40,7 @@ class Profile(models.Model):
     birthday = models.CharField(default='XXXX-XX-XX', max_length=10)
     current_employment = models.CharField(default='No Employment', max_length=20)
     account_type = models.CharField(default='Huntee', max_length=6)
-    profile_picture = models.ImageField(default='default/default.jpg', upload_to='media/profile_pics')
+    profile_picture = CloudinaryField('profile_picture')
     resume = models.FileField(upload_to='resumes', validators=[FileExtensionValidator(allowed_extensions=['pdf'])], verbose_name='Resume (.pdf only)', null=True, blank=True)
     website = models.URLField(default='', max_length=200, blank=True)
     privacy = models.CharField(default='Public', max_length=10)
@@ -50,13 +51,13 @@ class Profile(models.Model):
         return f'{self.user.username} Profile'
 
     # Save a Profile
-    def save(self):
-        super().save()
-        img = Image.open(self.profile_picture.path)
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.profile_picture.path)
+    #def save(self):
+        #super().save()
+        #img = Image.open(self.profile_picture.path)
+        #if img.height > 300 or img.width > 300:
+            #output_size = (300, 300)
+            #img.thumbnail(output_size)
+            #img.save(self.profile_picture.path)
 
 #
 # Model for a Comment on a Profile
