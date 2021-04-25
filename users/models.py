46ac4ba0f6be236d5890ae41a32c18575e1bb602
Noah Shaw <noahshaw11@yahoo.com>
2021-04-25 16:01:48 -0400

    Update models.py

--- users/models.py
+++ users/models.py
@@ -42,7 +42,7 @@ class Profile(models.Model):
     current_employment = models.CharField(default='No Employment', max_length=20)
     account_type = models.CharField(default='Huntee', max_length=6)
     profile_picture = CloudinaryField('Profile picture')
-    resume = CloudinaryField('Resume (.pdf only)', validators=[FileExtensionValidator(allowed_extensions=['doc', 'docx'])], resource_type='raw', null=True, blank=True)
+    resume = CloudinaryField('Resume (.pdf only)', validators=[FileExtensionValidator(allowed_extensions=['doc', 'docx'])], resource_type='raw', null=True)
     website = models.URLField(default='', max_length=200, blank=True)
     privacy = models.CharField(default='Public', max_length=10)
     has_unread_messages = models.BooleanField(default=False)
