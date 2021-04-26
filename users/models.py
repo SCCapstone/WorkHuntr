05a44f2d068e333e965c3ec7ae392756ae1ce883
Noah Shaw <noahshaw11@yahoo.com>
2021-04-26 16:27:27 -0400

    Update models.py

--- users/models.py
+++ users/models.py
@@ -50,15 +50,6 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
 
-    # Save a Profile
-    def save(self, **kwargs):
-        super().save()
-        img = Image.open(self.profile_picture.path)
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.profile_picture.path)
-
 #
 # Model for a Comment on a Profile
 #
