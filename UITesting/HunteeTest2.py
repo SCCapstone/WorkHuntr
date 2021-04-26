0d67667f92cea86674416f45d141698b70269db6
Matthewjre <matthewjre@yahoo.com>
2021-04-26 14:54:59 -0400

    Finished the UI tests with listing completions and payment tests

--- UITesting/HunteeTest2.py
+++ UITesting/HunteeTest2.py
@@ -7,9 +7,10 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 
 # WorkHuntr test login
+USERNAME = "WorkhuntrTester"
 USERNAMEHUNTER = "WorkhuntrHunter"
 PASSWORD = "Thispass142"
-GMAIL = "WorkhuntrHunter@gmail.com"
+GMAIL = "WorkhuntrTester@gmail.com"
 GMAILPASSWORD = "thispass142"
 testCount = 0
 failedTests = ""
@@ -19,12 +20,12 @@ driver = webdriver.Chrome("chromedriver.exe")
 print("RUNNING TESTS...")
 
 try:
-    print("RUNNING LOGIN TEST...")
+    print("Logging into the Huntee account...")
     # Go to login page
     urlLogin = "http://127.0.0.1:8000/login/"
     driver.get(urlLogin)
     # Find username field
-    driver.find_element_by_name("username").send_keys(USERNAMEHUNTER)
+    driver.find_element_by_name("username").send_keys(USERNAME)
     # Find password field
     driver.find_element_by_name("password").send_keys(PASSWORD)
     # Login
@@ -76,8 +77,6 @@ try:
     # if you don't wait for a second or two, selenium moves on before gmail registers the click on the trashcan
     time.sleep(2)
 
-
-    # Close gmail and switch the active tab back to workhuntr
     driver.close()
     driver.switch_to.window(driver.window_handles[0])#workhuntr
 
@@ -86,85 +85,51 @@ try:
     driver.find_element_by_id("loginButton").click()
 
     print("LOGIN SUCCESSFUL")
-    testCount += 1
 except:
     print("Login failed")
     failedTests += "\nlogin test fail\n"
 
 try:
-    testMessage = "Howdy, fella!"
-    print("TESTING MESSAGE RECEIVE / REPLY")
-    # Head over to message center
-    driver.find_element_by_xpath("//*[@id=\"navbarToggle\"]/div[1]/li/a").click()
-    # Check for the huntee's message
-    driver.find_element_by_xpath("/html/body/main/div/div/div/div/div/div/div/div/p[1]/a[1]").click()
-    # grab the message & check
-    hunteeMessage = driver.find_element_by_xpath("/html/body/main/div/div/div/div/div/p[2]").text
-    print("HUNTEE MESSAGE:",hunteeMessage)
-    if hunteeMessage == testMessage:
-        print("MESSAGE RECEIVE SUCCESSFUL")
-        testCount+=1
-    else:
-        print("MESSAGE RECEIVE FAIL")
-        failedTests += "\nreceive test fail\n"
-
-    # Attempt to reply
-    driver.find_element_by_xpath("//*[@id=\"message\"]").send_keys("Mornin. to ye, feller.")
-    driver.find_element_by_xpath("//*[@id=\"MSGsendBTN\"]").click()
-    # Check if sent
-    sender = driver.find_element_by_xpath("/html/body/main/div/div/div/div/div/p[1]").text
-
-    print("SENDER:",sender)
-    if sender == "WorkhuntrHunter":
-        print("MESSAGE REPLY SUCCESSFUL")
-        testCount += 1
-    else:
-        print("MESSAGE REPLY FAIL")
-        failedTests += "\nreply test fail\n"
-
-except:
-    print("MESSAGE RECEIVE / REPLY TEST FAIL")
-
-try:
-    print("TESTING LISTING CLAIM")
-
-    #Move to the listing tab and claim the Huntee's listing
+    print("RUNNING PAYMENT TEST")
+    # head to the listing tab
     driver.find_element_by_xpath("//*[@id=\"navbarToggle\"]/div[1]/a[2]").click()
-    driver.find_element_by_xpath("//*[@id=\"ListingsMain\"]/div[1]/table/tbody/tr[2]/td[5]/form/input[2]").click()
 
-    # Check for Success
-    textCheck = driver.find_element_by_xpath("/html/body/main/div/div/ul/p").text
+    # hit issue payment
+    driver.find_element_by_xpath("//*[@id=\"ListingsMain\"]/div[3]/table/tbody/tr[2]/td[5]/a").click()
 
-    if textCheck == "Listing CREATE THIS TEST has been marked claimed! WorkhuntrTester has been notified!":
-        print("LISTING CLAIM SUCCESSFUL")
-        testCount+=1
-    else:
-        print("LISTING CLAIM FAIL")
-        failedTests += "\nlisting claim test fail\n"
-except:
-    print("LISTING CLAIM FAIL")
-    failedTests += "\nlisting claim test fail\n"
+    # Fill out the payment form
+    driver.find_element_by_id("id_first_name").send_keys("Test")
+    driver.find_element_by_id("id_last_name").send_keys("Monkey")
+    driver.find_element_by_id("id_card_number").send_keys("1234567891234567")
+    driver.find_element_by_id("id_exp_month").send_keys("12")
+    driver.find_element_by_id("id_exp_year").send_keys("2025")
+    driver.find_element_by_id("id_cvv").send_keys("111")
+    driver.find_element_by_id("id_street_address").send_keys("Im not putting my address")
+    driver.find_element_by_id("id_billing_city").send_keys("columbia")
+    driver.find_element_by_id("id_zip").send_keys("12345")
 
-try:
-    print("TESTING LISTING COMPLETION")
-    # click on the claimed to start updating the status
-    driver.find_element_by_xpath("//*[@id=\"ListingsMain\"]/div[2]/table/tbody/tr[2]/td[4]/a").click()
+    # hit issue payment
+    driver.find_element_by_xpath("/html/body/main/div/div/div/form/input[2]").click()
 
-    # set the status to complete and give a description.
-    dropdown = Select(driver.find_element_by_name("status"))
-    dropdown.select_by_value("Complete")
-    driver.find_element_by_id("id_description").send_keys("This is a test description")
-    driver.find_element_by_xpath("//*[@id=\"updateForm\"]/form/fieldset/input").click()
-
-    print("LISTING COMPLETION SUCCESSFUL")
+    print("PAYMENT TEST SUCCESS")
     testCount+=1
-
 except:
-    print("LISTING COMPLETION FAIL")
-    failedTests += "\nlisting completion test fail\n"
+    print("PAYMENT TEST FAIL")
+    failedTests += "\npayment test fail\n"
 
 print("\n\n" + "Number of test run: " + str(testCount) + " + Account creation test.")
 print("\nFailed tests:\n"+failedTests)
+
+# Clean up
+
+# Delete the listing so it doesn't get in the way of future tests
+driver.find_element_by_xpath("//*[@id=\"ListingsMain\"]/div[4]/table/tbody/tr[2]/td[5]/a").click()
+driver.find_element_by_xpath("/html/body/main/div/div/div/form/input[2]").click()
+
+# Head to the messages and delete them
+driver.find_element_by_xpath("//*[@id=\"navbarToggle\"]/div[1]/li/a").click()
+driver.find_element_by_xpath("/html/body/main/div/div/div/div/div/div/div/div/p[1]/a[2]").click()
+driver.find_element_by_xpath("/html/body/main/div/div/div/div/form/button").click()
 
 driver.switch_to.window(driver.window_handles[0])  # workhuntr
 driver.close()
