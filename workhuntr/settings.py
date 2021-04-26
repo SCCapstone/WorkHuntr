55a76e13fe803d91c669fe0b6f166ad81255e33c
Noah Shaw <noahshaw11@yahoo.com>
2021-04-25 21:14:23 -0400

    Update settings.py

--- workhuntr/settings.py
+++ workhuntr/settings.py
@@ -31,7 +31,7 @@ STATIC_URL = '/static/'
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'workhuntr.herokuapp.com']
 
