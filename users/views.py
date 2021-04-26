4cdf5f7ca8ae8ba0f6826cbcec468479ec0e0601
Noah Shaw <noahshaw11@yahoo.com>
2021-04-25 20:45:09 -0400

    Update views.py

--- users/views.py
+++ users/views.py
@@ -68,15 +68,10 @@ def edit_profile(request, username):
     if request.method == 'POST':
         u_form = UserUpdateForm(request.POST, instance=request.user)
         p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
-        if u_form.is_valid():
+        if u_form.is_valid() and p_form.is_valid():
             u_form.save()
-        if p_form.is_valid():
-            print("RACHEAJDKFJAKLJFKJ")
-            profile = p_form.save(commit=False)
-            profile.privacy = p_form.cleaned_data.get('privacy')
-            profile.save()
+            p_form.save()
             messages.success(request, f'Your account has been updated!')
-        print("HERE WE ARE")
         return redirect('profile', username)
     else:
         u_form = UserUpdateForm(instance=request.user)
